bb448b26b1e2bfafe920cc921cef7c2229fc0207
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-06-21 16:03:52 +0300

    add form for send message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,15 @@
 class MessagesController < ApplicationController
   before_action :conversation
 
+  def create
+    receipt = current_user.reply_to_conversation(@conversation, params[:body])
+    redirect_to conversation_path receipt.conversation
+  end
+
   private
 
   def conversation
-    @conversation = current_user.mailbox.conversations.find(params[:conversation_id])
+    @conversation = current_user.mailbox.conversations
+                                .find(params[:conversation_id])
   end
 end
