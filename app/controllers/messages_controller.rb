e7317cd7c0374b399b33e523bdeb7d57f68ae6cd
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-06-21 15:31:46 +0300

    Add hook conversation in messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,9 @@
 class MessagesController < ApplicationController
+  before_action :conversation
+
+  private
+
+  def conversation
+    @conversation = current_user.mailbox.conversations.find(params[:conversation_id])
+  end
 end
