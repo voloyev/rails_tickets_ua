e4e13287f22d7a2ede612cff7bcbb16e03ec7cbb
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-06-21 15:28:25 +0300

    Add index and show actions in conversations controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,2 +1,11 @@
 class ConversationsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @conversations = current_user.mailbox.conversations
+  end
+
+  def show
+    @conversation = current_user.mailbox.conversations.find(params[:id])
+  end
 end
