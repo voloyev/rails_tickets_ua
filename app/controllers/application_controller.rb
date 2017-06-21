3c293ce768730d93a76300aab4db56b65003ab69
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-06-22 02:13:50 +0300

    Add rescue if no user is in database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound do
+    flash[:warning] = 'Resource not found.'
+    redirect_back_or root_path
+  end
+
+  def redirect_back_or(path)
+    redirect_to request.referer || path
+  end
 end
