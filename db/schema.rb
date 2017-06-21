273083561cd0cb282ba333d935afbcce35b9d46c
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-06-21 23:10:19 +0300

    Add name field to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170621115847) do
+ActiveRecord::Schema.define(version: 20170621194855) do
 
   create_table "mailboxer_conversation_opt_outs", force: :cascade do |t|
     t.string "unsubscriber_type"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 20170621115847) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
