3338753c46b8e41337e5b1526a5293a2d226f589
Volodymyr Yevtushenko <yevova10@gmail.com>
2017-06-22 02:10:13 +0300

    Add actions

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,14 +1,21 @@
 class ConversationsController < ApplicationController
   before_action :authenticate_user!
+  before_action :mailbox
+  before_action :conversation, except: [:index]
+  before_action :box, only: [:index]
 
   def index
-    @conversations = current_user.mailbox.conversations
-    @unread = unread_messages
+    if @box.eql? 'inbox'
+      @conversations = @mailbox.inbox
+    elsif @box.eql? 'sent'
+      @conversations = @mailbox.sentbox
+    else
+      @conversations = @mailbox.trash
+    end
+    @conversations = @conversations.paginate(page: params[:page], per_page: 10)
   end
 
-  def show
-    @conversation = current_user.mailbox.conversations.find(params[:id])
-  end
+  def show; end
 
   def new
     @recipients = User.all - [current_user]
@@ -20,15 +27,44 @@ class ConversationsController < ApplicationController
     redirect_to conversation_path receipt.conversation
   end
 
+  def reply
+    current_user.reply_to_conversation(@conversation, params[:body])
+    flash[:success] = 'Reply sent'
+    redirect_to conversation_path(@conversation)
+  end
+
+  def destroy
+    @conversation.move_to_trash(current_user)
+    flash[:success] = 'The conversation was moved to trash.'
+    redirect_to conversations_path
+  end
+
+  def restore
+    @conversation.untrash(current_user)
+    flash[:success] = 'The conversation was restored.'
+    redirect_to conversations_path
+  end
+
+  def mark_as_read
+    @conversation.mark_as_read(current_user)
+    flash[:success] = 'The conversation was marked as read.'
+    redirect_to conversations_path
+  end
+  
   private
 
-  def unread_messages
-    @unread = 0
-    @conversations.map do |c|
-      c.messages.each do |m|
-        @unread += 1 if m.is_unread?(current_user)
-      end
+  def mailbox
+    @mailbox ||= current_user.mailbox
+  end
+
+  def conversation
+    @conversation ||= @mailbox.conversations.find(params[:id])
+  end
+
+  def box
+    if params[:box].blank? || !%w[inbox sent trash].include?(params[:box])
+      params[:box] = 'inbox'
     end
-    @unread
+    @box = params[:box]
   end
 end
